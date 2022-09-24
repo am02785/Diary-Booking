ccef269583464d6e94a772294bb46b2307baa078
git <ar.matoorian@gmail.com>
2022-09-24 17:04:10 +0100

    added the Booking class

--- DiaryBooking/User.cs
+++ DiaryBooking/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DiaryBooking
 {
@@ -8,6 +9,7 @@ namespace DiaryBooking
         private string surname;
         private string age;
         private string email;
+        private List<Booking> bookings;
 
         public User(string forename, string surname, string age, string email)
         {
@@ -15,6 +17,13 @@ namespace DiaryBooking
             this.surname = surname;
             this.age = age;
             this.email = email;
+            this.bookings = new List<Booking>();
+        }
+
+        public void addBooking(string name, DateTime startTime, DateTime endTime)
+        {
+            Booking booking = new Booking(name, startTime, endTime);
+            this.bookings.Add(booking);
         }
 
     }
