9dc00f179d1919b46fd85adb342a5234a93d1922
git <ar.matoorian@gmail.com>
2022-09-24 16:18:29 +0100

    created the user class

---
+++ DiaryBooking/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DiaryBooking
+{
+    class User
+    {
+        private string forename;
+        private string surname;
+        private string age;
+        private string email;
+
+        public User(string forename, string surname, string age, string email)
+        {
+            this.forename = forename;
+            this.surname = surname;
+            this.age = age;
+            this.email = email;
+        }
+
+    }
+}
